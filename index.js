41a99fee45129aa6fe3b5b2481c3f16e3e72ad9e
m-abdul-mukhlis <61910707+m-abdul-mukhlis@users.noreply.github.com>
2022-12-05 10:59:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require("body-parser");
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/src', express.static('src'))
+
+const domain = "https://telegra.ph/"
+
+app.get('/', (req, res) => {
+  res.sendFile("index.html", { root: '.' })
 })
+
+app.post('/api/post_images', (req, res) => {
+  const url = req?.body?.url
+  axios(url).then((response) => {
+    const $ = cheerio.load(response.data);
+    const pic = [];
+    const pictures = $('figure');
+    const title = folderNamePreprocess($('title').text());
+    for (let i = 0; i < pictures.length; i++) {
+      let src = domain + pictures[i.toString()].children[0].attribs.src;
+      pic.push({ src: src, title: title });
+    }
+    res.status(400).json({ message: 'success', result: pic })
+  }).catch((e) => {
+    res.status(400).json({ message: e, result: {} })
+  })
+})
+
+const folderNamePreprocess = (folder_name) => {
+  folder_name = folder_name.replace(/[ |?<\\*:"']/g, '');
+  return folder_name;
+}
+
 app.listen(process.env.PORT || 3000)
